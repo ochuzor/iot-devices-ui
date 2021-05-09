14aff32ffa1392eef826fb2a547be99a2795dc84
Chinedu Eze <ochuzor@gmail.com>
2021-05-09 16:08:58 +0300

    add state badge

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Badge from '@material-ui/core/Badge';
 import Grid from '@material-ui/core/Grid';
 import { DataGrid, GridColDef, GridValueGetterParams, GridRowModel } from '@material-ui/data-grid';
 
@@ -24,58 +25,23 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'State', width: 80 },
+  {
+    field: 'id',
+    headerName: 'State',
+    width: 80,
+    sortable: false,
+    renderCell: () => <Badge color="error" variant="dot" />,
+  },
   {
     field: 'nameId',
     headerName: 'Name/ID',
     width: 300,
     valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.name || ''} ${params.getValue('id') || ''}`,
+      `${params.row.name || ''} ${params.row.id || ''}`,
   },
   { field: 'model', headerName: 'Model', width: 130 },
   { field: 'serialNumber', headerName: 'Serial', width: 130 },
-
-  /*
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  }, // */
 ];
-
-/*
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  { id: 11, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 12, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 13, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 14, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 15, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 16, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 17, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 18, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 19, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-// */
 
 const rows = [
   {
@@ -137,7 +103,6 @@ export default function App() {
               />
             </div>
           </Paper>
-          
         </Grid>
       </div>
     </>
